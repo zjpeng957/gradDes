221ce0e2281f43345c8386ed7c91abc0c8fd6985
zjpeng957 <1528580043@qq.com>
2017-07-29 16:40:56 +0800

    logistic regression using gradient descent

--- gd.py
+++ gd.py
@@ -1,34 +1,42 @@
 import numpy as np
 import data
 import matplotlib.pyplot as plt
+import math
+
+
+def sigmoid (x, theta):
+    z = np.dot (x, theta)
+    d, c = np.shape (z)
+    h = np.reciprocal (np.ones ((d, 1)) + np.exp (np.negative (z)))
+    return h
 
 
 def batchGradientDescent (x, y, theta, alpha, max_N):
-    xT = x.transpose()
-    yT=y.transpose()
-    for i in range(0, max_N):
-        h = np.dot(x, theta)
-        loss = h - y
-        gradient = np.dot(xT, loss)
-        theta = theta - alpha * gradient
+    xT = x.transpose ()
+    for i in range (0, max_N):
+        h = sigmoid (x, theta)
+        loss = y - h
+        gradient = np.dot (xT, loss)
+        theta = theta + alpha * gradient
     return theta
 
 
-def predict(x, theta):
-    y = np.dot(x, theta)
-    return y
+def predict (x, theta):
+    y = sigmoid (x, theta)
+    w = y > 0.5
+    return w
 
 
 if __name__ == "__main__":
-    t_data, t_label = data.readData()
-    t_label=np.array([[16],[24],[38],[25],[21],[41]])
-    the=np.zeros((3, 1))
-    alp=0.001
-    MAX_N=50
-    the = batchGradientDescent(t_data, t_label, the, alp, MAX_N)
+    t_data, t_label = data.readData ()
+    t_label = np.array ([[0], [0], [1], [0], [0], [1],[0],[0],[0],[1],[1],[0],[1]])
+    the = np.zeros ((3, 1))
+    alp = 0.001
+    MAX_N = 1000
+    the = batchGradientDescent (t_data, t_label, the, alp, MAX_N)
 
-    xp=np.array([[1,1,2],[1,5,6],[1,0,0],[1,32,56]])
-    result=predict(xp,the)
-    plt.plot([1,2,3,4],result)
-    print(result)
-    plt.show()
+    xp = np.array ([[1, 1, 2], [1, 5, 6], [1, 0, 0], [1, 32, 56]])
+    result = predict (xp, the)
+    plt.plot ([1, 2, 3, 4], result)
+    print (result)
+    plt.show ()
